f197d9dd04d35bbe170b48af116c0d3f82c50055
chinny CircuitNight <jangmaleo@gmail.com>
2026-02-23 01:31:26 +0700

    chore: remove dist from version control

--- backend/dist/shared/types/index.js
+++
@@ -1,7 +0,0 @@
-export * from "./user.js";
-export * from "./product.js";
-export * from "./order.js";
-export * from "./api.js";
-export * from "./admin.js";
-export * from "./cart.js";
-//# sourceMappingURL=index.js.map
